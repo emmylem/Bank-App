c9e338913a70ea8a3ef135dc03ef6ec4cd30dc46
emmylem <106612291+emmylem@users.noreply.github.com>
2024-09-20 09:43:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     def __init__(self, username, password):
         self.username = username
